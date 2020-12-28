5dd5a82640af2a91257a38166cdd0ad61d80377b
xianzaji <xianzaji@163.com>
2020-12-29 01:31:34 +0800

    details for virtualbox port forwarding config

--- restfulapi/run.py
+++ restfulapi/run.py
@@ -7,4 +7,11 @@ def index():
 	return 'hello world3'
 
 if __name__=="__main__":
-	app.run(host='0.0.0.0',debug=True)
+	app.run(host='10.0.2.15',debug=True)
+
+# 端口转发配置：TCP 192.168.56.1 5000 10.0.2.15 5000
+# 以太网适配器 VirtualBox Host-Only Network:
+
+#    连接特定的 DNS 后缀 . . . . . . . :
+#    本地链接 IPv6 地址. . . . . . . . : fe80::e8a1:e016:714b:4181%18
+#    IPv4 地址 . . . . . . . . . . . . : 192.168.56.1
